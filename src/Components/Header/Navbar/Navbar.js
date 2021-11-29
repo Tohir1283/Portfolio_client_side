0c908b1396431a7fc0f883c5225d0fb9c025e80e
Tohir1283 <tohirul.islam164@gmail.com>
2021-11-30 00:35:08 +0600

    nav title

--- src/Components/Header/Navbar/Navbar.js
+++ src/Components/Header/Navbar/Navbar.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-undef */
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
@@ -5,7 +6,14 @@ import "./Navbar.css";
 const Navbar = () => {
   return (
     <div>
-      <div className="title"></div>
+      <nav className="navbar "></nav>
+      <div className="title">
+        <h2>
+          <pre>
+            <code>{`${"<" + "Tohirul Islam" + " />"}`}</code>
+          </pre>
+        </h2>
+      </div>
       <div className="navbar-links">
         <ul>
           <li>
