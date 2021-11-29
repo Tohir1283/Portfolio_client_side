67ba3a6de3e6b9e648da52a2ae8c6a1f7df02d06
Tohir1283 <tohirul.islam164@gmail.com>
2021-11-30 00:52:13 +0600

    nav

--- src/Components/Header/Navbar/Navbar.js
+++ src/Components/Header/Navbar/Navbar.js
@@ -6,23 +6,24 @@ import "./Navbar.css";
 const Navbar = () => {
   return (
     <div>
-      <nav className="navbar "></nav>
-      <div className="title">
-        <h2>
-          <pre>
-            <code>{`${"<" + "Tohirul Islam" + " />"}`}</code>
-          </pre>
-        </h2>
-      </div>
-      <div className="navbar-links">
-        <ul>
-          <li>
-            <Link to="/home">Home</Link>
-            <Link to="/projects">Projects</Link>
-            <Link to="/contact">Contact Me</Link>
-          </li>
-        </ul>
-      </div>
+      <nav className="navbar ">
+        <div className="title">
+          <h2>
+            <pre>
+              <code>{`${"<" + "Tohirul Islam" + " />"}`}</code>
+            </pre>
+          </h2>
+        </div>
+        <div className="navbar-links">
+          <ul>
+            <li>
+              <Link to="/home">Home</Link>
+              <Link to="/projects">Projects</Link>
+              <Link to="/contact">Contact Me</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </div>
   );
 };
