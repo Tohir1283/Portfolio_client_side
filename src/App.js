6d16986b9829c7dc9e751d414e5aa16bd69cf6cb
Tohir1283 <tohirul.islam164@gmail.com>
2021-12-18 05:56:37 +0600

    preloader: setLoading

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./Pages/Home/Home";
@@ -6,32 +6,37 @@ import Navbar from "./Components/Navbar/Navbar";
 import LoadScreen from "./Components/LoadScreen/LoadScreen";
 
 function App() {
+  const [loading, setLoading] = useState(false);
   document.onreadystatechange = function () {
     if (document.readyState !== "complete") {
-      document.getElementById("app").style.visibility = "hidden";
-      document.getElementById("loadingScreen").style.visibility = "visible";
+      setLoading(true);
     } else if (document.readyState === "complete") {
-      document.getElementById("loadingScreen").style.display = "none";
-      document.getElementById("app").style.visibility = "visible";
+      setTimeout(() => {
+        setLoading(false);
+      }, 3000);
     }
   };
   return (
-    <div>
-      <div id="loadingScreen">
-        <LoadScreen />
-      </div>
-      <div className="App" id="app">
-        <BrowserRouter>
-          <Navbar />
-          <Switch>
-            <Route exact path="/" component={Home}></Route>
-            <Route exact path="/home" component={Home}></Route>
-            <Route exact path="/projects"></Route>
-            <Route exact path="/contact"></Route>
-            <Route exact path="/about"></Route>
-          </Switch>
-        </BrowserRouter>
-      </div>
+    <div style={{ background: "black" }}>
+      {loading && (
+        <div id="loadingScreen">
+          <LoadScreen />
+        </div>
+      )}
+      {!loading && (
+        <div className="App" id="app">
+          <BrowserRouter>
+            <Navbar />
+            <Switch>
+              <Route exact path="/" component={Home}></Route>
+              <Route exact path="/home" component={Home}></Route>
+              <Route exact path="/projects"></Route>
+              <Route exact path="/contact"></Route>
+              <Route exact path="/about"></Route>
+            </Switch>
+          </BrowserRouter>
+        </div>
+      )}
     </div>
   );
 }
