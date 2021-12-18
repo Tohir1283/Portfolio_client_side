30251fa3ffcf172c9793035f4ed90be5d438b896
Tohir1283 <tohirul.islam164@gmail.com>
2021-12-18 18:12:14 +0600

    loader: background

--- src/Components/LoadScreen/LoadScreen.js
+++ src/Components/LoadScreen/LoadScreen.js
@@ -9,14 +9,14 @@ const LoadScreen = () => {
     `;
 
   const Div = styled.div`
-    background-color: #fff;
+    background-color: #000000;
     width: 16px;
     height: 32px;
     margin-left: 10px;
     animation: ${loading} 1.2s infinite;
   `;
   const Section = styled.section`
-    background: #111;
+    background: #000000;
     width: 100%;
     height: 100vh;
     display: flex;
